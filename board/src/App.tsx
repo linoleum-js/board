a71b3ec1613e78074f6d6f0644b1dc924857030e
m.schulz <m.schulz@artlogics.ru>
2020-03-29 14:17:52 +0500

    init

---
+++ board/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Board } from './components/Board/Board';
+
+function App() {
+  return (
+    <Board />
+  );
+}
+
+export default App;
---
+++ board/src/components/Board/Board.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export interface IBoardProps {
+
+}
+
+export const Board: React.FunctionComponent<IBoardProps> = () => {
+  return <div>
+    board
+  </div>;
+};
